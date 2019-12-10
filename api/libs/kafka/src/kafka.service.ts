b33e84b397634e631e72d9d17afc73992e8278d1
Jari Möllenbernd <jari@moellenbernd.de>
2019-12-10 22:23:58 +0100

    feat(notification): implement prototype of kafka consumer

--- api/libs/kafka/src/kafka.service.ts
+++ api/libs/kafka/src/kafka.service.ts
@@ -1,8 +1,38 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Kafka } from 'kafkajs';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class KafkaService implements OnModuleInit {
+  private kafka: Kafka;
+
   onModuleInit() {
-    console.log('initialize kafka');
+    this.kafka = new Kafka({
+      clientId: 'boxolo-api',
+      brokers: ['localhost:9092'],
+    });
+  }
+
+  createConsumer(groupId: string, topic: string): Observable<any> {
+    const consumer = this.kafka.consumer({ groupId });
+    return Observable.create(observer =>
+      consumer
+        .connect()
+        .then(() => consumer.subscribe({ topic, fromBeginning: true }))
+        .then(() =>
+          consumer.run({
+            eachMessage: async ({ partition, message }) => {
+              observer.next({
+                partition,
+                offset: message.offset,
+                value: message.value.toString(),
+              });
+            },
+          }),
+        )
+        .catch(error => {
+          observer.error(error);
+        }),
+    );
   }
 }
--- api/src/notification/notification.service.ts
+++ api/src/notification/notification.service.ts
@@ -1,7 +1,34 @@
 import { KafkaService } from '@jarmee/kafka';
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { map, withLatestFrom } from 'rxjs/operators';
+import { Notification } from './notification.model';
 
 @Injectable()
-export class NotificationService {
+export class NotificationService implements OnModuleInit, OnModuleDestroy {
+  notifications$: BehaviorSubject<Notification[]> = new BehaviorSubject<
+    Notification[]
+  >([]);
+
+  private notificationSubscription: Subscription;
+
   constructor(private readonly kafkaService: KafkaService) {}
+
+  onModuleInit() {
+    this.notificationSubscription = this.kafkaService
+      .createConsumer('notification-group', 'notification')
+      .pipe(
+        map(({ value: data }) => ({ data })),
+        withLatestFrom(this.notifications$),
+        map(([notification, notifications]) => [
+          ...notifications,
+          notification,
+        ]),
+      )
+      .subscribe(notifications => this.notifications$.next(notifications));
+  }
+
+  onModuleDestroy() {
+    this.notificationSubscription.unsubscribe();
+  }
 }
